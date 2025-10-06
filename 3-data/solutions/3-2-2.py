11f78905cc9227b66ad195ba96da26cd4ef77924
Angela <auramnar@syr.edu>
2025-10-06 07:17:21 -0400

    Update 3-2-2.py

--- 3-data/solutions/3-2-2.py
+++ 3-data/solutions/3-2-2.py
@@ -6,5 +6,6 @@ st.title("Employee JSON")
 
 response = requests.get("https://raw.githubusercontent.com/mafudge/datasets/master/json-samples/employees.json")
 employees = response.json()
+st.write(employees)
 employees_df = pd.json_normalize(employees, record_path=["employees"], meta=["dept"])
 st.dataframe(employees_df)
